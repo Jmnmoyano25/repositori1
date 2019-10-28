c7679ffe7085ee1c9449f18746928f64b628ebec
alumnot <alumnot@edhost>
2019-10-28 19:55:46 +0100

    mi primer commit al rep

---
+++ Miproyecto de Prueba/src/misProyectos/Primero.java
@@ -0,0 +1,10 @@
+package misProyectos;
+
+public class Primero {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("que hay de nuevo viejo!");
+	}
+
+}
